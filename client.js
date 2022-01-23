726ac9894f477656f33d405b3ab8814799d7d740
Aekkasitrun <aekkasitrun@gmail.com>
2022-01-23 23:06:58 +0700

    comit 3

--- client.js
+++ client.js
@@ -1,3 +1,6 @@
+var readlineSync = require("readline-sync");
+var colors = require("colors");
+
 var net = require('net');
 var HOST = '127.0.0.1';
 var PORT = 6969;
@@ -6,7 +9,7 @@ client.connect(PORT, HOST, function() {
     console.log('CONNECTED TO: ' + HOST + ':' + PORT);
     // Write a message to the socket as soon as the client is connected,
     //the server will receive it as message from the client 
-    client.write('I am Chuck Norris!');
+    client.write('HELLO');
 });
 // Add a 'data' event handler for the client socket
 // data is what the server sent to this socket
---
+++ client_11.js
@@ -0,0 +1,37 @@
+var readlineSync = require("readline-sync");
+//var colors = require("colors");
+var net = require("net");
+
+var HOST = "127.0.0.1";
+var PORT = 6969;
+
+var client = null;
+
+function menu() {
+    var lineRead = readlineSync.question("\n\nEnter Option (1-Open, 2-Send, 3-Close, 4-Quit):");
+
+    switch (lineRead) {
+        case "1":
+            console.log("Option 1 selected");
+            break;
+        case "2":
+            console.log("Option 2 selected");
+            break;
+        case "3":
+            console.log("Option 3 selected");
+            break;
+        case "4":
+            console.log("Option 4 selected");
+            break;
+        default:
+            setTimeout(function () {
+                menu();
+            }, 0);
+            break;
+
+    }
+}
+
+setTimeout(function () {
+    menu();
+}, 0);
